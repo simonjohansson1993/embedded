d9d479134ecaedcb60ad009681dffa954a42ddf4
simon johansson <45367329+simonjohansson1993@users.noreply.github.com>
2020-02-08 12:38:31 +0100

    Add files via upload

---
+++ wp3/cmake-build-debug/CMakeFiles/wp3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wp3.dir/exerc__3_1.c.obj"
+  "libwp3.dll.a"
+  "wp3.exe"
+  "wp3.exe.manifest"
+  "wp3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/wp3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
